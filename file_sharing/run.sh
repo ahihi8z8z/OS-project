73e165e1bc234a60800d461f98e96fc0853d218a
ahihi8z8z <60187487+ahihi8z8z@users.noreply.github.com>
2022-11-28 15:58:08 +0700

    Update run.sh

--- file_sharing/run.sh
+++ file_sharing/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-for i in {1..100}
+echo -e "Ket qua do se co dang sau : \n Time process 1 start write to file: %ld second + %ld nanosecond \n Time process 1 stop write to file: %ld second + %ld nanosecond \n Time process 2 start read from file: %ld second + %ld nanosecond \n Time process 2 stop read from file: %ld second + %ld nanosecond \n" >> time_file.txt
+for i in {1..50}
 do 
 	echo -e "Lan do thu $i: " >> time_file.txt
 	./FileSharing "test/test$i"
