52acc2f3853a48a9e9f4f84544b7de90c99614a0
ahihi8z8z <60187487+ahihi8z8z@users.noreply.github.com>
2022-11-28 18:50:14 +0700

    Update SharedMemory.c

--- shared_memory/SharedMemory.c
+++ shared_memory/SharedMemory.c
@@ -1,74 +1,111 @@
 #include <stdio.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>	
+#include <assert.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <pthread.h>
+#include <time.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-const char content[] = "1a7qrN99lFPZwnPzH47H7uow5irH0moY3akriTiVCCkkDMT7lLa6AGK5X8iOHxkUerGAYQiEY3dPiwafQKKsSRgydthjUbSJTak3WmMmkXm8NfuGOhH0eBSCrpHDeZbUsdb6O9rZZo0eJXeyCP7URA776nyYhmb644vdR6cbFAARjNIBBzp0fDjFDJms4aSooE945sFEXqn1tn6ztJUnvmQIY85AbUNZdyajQhazgJVcsfuVH331sH5FJrE05QrUCd8E5n1aH1nnz4y9LhjSYfdSDVGxkrUWtZ1mOIDBdGL0o1mZdveyFxcJunTEKyYKf04jv8XNemTx5oFC6D5s0Yv9nyzrtNnIlwwOulXZZQCMymNDnw8WQvl8C2ZsYDHYF7ySdw3KNaRlurL4lKWzPBNQ4FohVNli6y3oOhrc59maba9qcPt7DqxQRBzuCDJo0cwSefpeUQTG0DGJYtCMqDnVpVeCAycEqr1zdyUSAQtjT8Wboy0dG2l3slDAQZbUwaky4yh9eSffjlsFtZX2dd9rj9eEZtkh6Ets5hHHwp3ZngUnAcGQXoh8EXNuNOSbDLmoj3H8FSCHKU4bcxKujCBcrBGA69ms1Forj0YNjUhQJhXmFc3A78zbb9indyV8mTnn47fUsbnf5hc4p72pAX1bxBrZBMLrN0ZDT7HgKB1Ntj5PXUVtUXvf4GLApxB68iCF8bX0OcqKud4lBBryFIsVZ584p8ra2dW23qRx0ChmKFPXPqd2aHc4ZMWCaf114wqXGmjGymalKV6OzvmzNogGB0H9iE89GbyJ5vSvfbE3Dbr0GX2KR2LaN2ijEEJfXXhjq7fI4kpcqjVzmboOo8aE56xj6FV4UwIPr7GQkm9P0O6RqxzQyhP1LWn7Fdw9s7JdIwrc4Fmpy5HeqKpH1exBDHDnAXDZdJhppW89Ksdr8p1HpkEV4SqqXJrmTKXs8wlwm0ShM3LhCxGjqXZuwfRcFGAaLz8XDTyhuG6TC1bojUh0v9HNyqyrttu01hF1cAMScJ8dL1Aqh96OZou7WX86dF1LdaK57oH423Ug9sXgd56SiUB3aNEjjjpl0qvl2sfj2mF38uQzZLZUGaNHY7hTD67RQ3Lv1HwtEAqTUGtoYeAGv4DwTORcjasDzPB0AVLL49zrlGjAIRe7v6eYqi8PubwjrWP21JgwMls3RS8df7Smrfdrjw51rVwiZmUynX1QuQFggnHzlOeOLssLDBMHD6P7csKaxdvubbvQCAr4DTw6Wcm5BrgbG7IjpqRvBbfUzpblIrvVrqGFqqQoaqjFG8CHUIjVkWBqU3p124FJCZTCxVlFAI2L8t7Cz4GZ6Q0TGFx7JUp0K7UXf76B0AQhWxJxosmeGA5yFUPQyhfbw3cdIEawxEAQldsfQ1Sa54quIuvDIzjnnUBYXv2bjKnwQLumXDGFaryo4WacXk7tznF9xP6YgAkBh2JyDekz9BN0qFVsGDxQgMKaSyAI3EEeKq4Tw5EIlQcIWsSbUb56EAKiSeCDERl3FkUrQDXb37TNSkBa1Vw4rOCpwLNRxs3JnUEptguOujE8JcSMkR2tFg0uZZ4Fa2HUydznk4Y23RFEnjeiwT3AngeqCt5u3WqOY7euy2aANSCaR6BJ4icLUwPqb38PIjryhFmjz6S1jzu5rRI8lgMukFQBylttvlViQsiwERTPg6pqfeRIfZFzfAjmgtiq6kUd7Vfui6aLyPzKh5ZtUyOzc07f6bzAfRkCb1G2I9PJDaT1qcEauhWb7mGwF3vpBMOfv3mtUU6ws9qGuj3wAW4i7IJxlsC2tk4LVX1JpKB4MiosPAbpWxhr3rstQNQYrkNZDuKXujK2AfB15Fida2TtJyMtoBuod0DfQwDKYrtjWyWVBbzNPsdkHyCL4Kye1o2QqRU7ZPmrYiGWiG3A98DbQGTZijQ2wb6SjysDkUekMPLAZA4dBNifMjxF7opU2D3mj1vQfUKdgSII31EKAQ74eM2JgEOQIyAhhlR6GKZkl0KI9buqGrPrS7m9CMpZNMoJRlPtzU0n";
-size_t contentLen  = sizeof(content)/sizeof(content[0]) ;
+const char content[] = "1a7qrN99lFPZnPzH47H7uow5irH0moY3akriTiVCCkkDMT7lLa6AGK5X8iOHxkUerGAYQiEY3dPiwafQKKsSRgydthjUbSJTak3WmMmkXm8NfuGOhH0eBSCrpHDeZbUsdb6O9rZZo0eJXeyCP7URA776nyYhmb644vdR6cbFAARjNIBBzp0fDjFDJms4aSooE945sFEXqn1tn6ztJUnvmQIY85AbUNZdyajQhazgJVcsfuVH331sH5FJrE05QrUCd8E5n1aH1nnz4y9LhjSYfdSDVGxkrUWtZ1mOIDBdGL0o1mZdveyFxcJunTEKyYKf04jv8XNemTx5oFC6D5s0Yv9nyzrtNnIlwwOulXZZQCMymNDnw8WQvl8C2ZsYDHYF7ySdw3KNaRlurL4lKWzPBNQ4FohVNli6y3oOhrc59maba9qcPt7DqxQRBzuCDJo0cwSefpeUQTG0DGJYtCMqDnVpVeCAycEqr1zdyUSAQtjT8Wboy0dG2l3slDAQZbUwaky4yh9eSffjlsFtZX2dd9rj9eEZtkh6Ets5hHHwp3ZngUnAcGQXoh8EXNuNOSbDLmoj3H8FSCHKU4bcxKujCBcrBGA69ms1Forj0YNjUhQJhXmFc3A78zbb9indyV8mTnn47fUsbnf5hc4p72pAX1bxBrZBMLrN0ZDT7HgKB1Ntj5PXUVtUXvf4GLApxB68iCF8bX0OcqKud4lBBryFIsVZ584p8ra2dW23qRx0ChmKFPXPqd2aHc4ZMWCaf114wqXGmjGymalKV6OzvmzNogGB0H9iE89GbyJ5vSvfbE3Dbr0GX2KR2LaN2ijEEJfXXhjq7fI4kpcqjVzmboOo8aE56xj6FV4UwIPr7GQkm9P0O6RqxzQyhP1LWn7Fdw9s7JdIwrc4Fmpy5HeqKpH1exBDHDnAXDZdJhppW89Ksdr8p1HpkEV4SqqXJrmTKXs8wlwm0ShM3LhCxGjqXZuwfRcFGAaLz8XDTyhuG6TC1bojUh0v9HNyqyrttu01hF1cAMScJ8dL1Aqh96OZou7WX86dF1LdaK57oH423Ug9sXgd56SiUB3aNEjjjpl0qvl2sfj2mF38uQzZLZUGaNHY7hTD67RQ3Lv1HwtEAqTUGtoYeAGv4DwTORcjasDzPB0AVLL49zrlGjAIRe7v6eYqi8PubwjrWP21JgwMls3RS8df7Smrfdrjw51rVwiZmUynX1QuQFggnHzlOeOLssLDBMHD6P7csKaxdvubbvQCAr4DTw6Wcm5BrgbG7IjpqRvBbfUzpblIrvVrqGFqqQoaqjFG8CHUIjVkWBqU3p124FJCZTCxVlFAI2L8t7Cz4GZ6Q0TGFx7JUp0K7UXf76B0AQhWxJxosmeGA5yFUPQyhfbw3cdIEawxEAQldsfQ1Sa54quIuvDIzjnnUBYXv2bjKnwQLumXDGFaryo4WacXk7tznF9xP6YgAkBh2JyDekz9BN0qFVsGDxQgMKaSyAI3EEeKq4Tw5EIlQcIWsSbUb56EAKiSeCDERl3FkUrQDXb37TNSkBa1Vw4rOCpwLNRxs3JnUEptguOujE8JcSMkR2tFg0uZZ4Fa2HUydznk4Y23RFEnjeiwT3AngeqCt5u3WqOY7euy2aANSCaR6BJ4icLUwPqb38PIjryhFmjz6S1jzu5rRI8lgMukFQBylttvlViQsiwERTPg6pqfeRIfZFzfAjmgtiq6kUd7Vfui6aLyPzKh5ZtUyOzc07f6bzAfRkCb1G2I9PJDaT1qcEauhWb7mGwF3vpBMOfv3mtUU6ws9qGuj3wAW4i7IJxlsC2tk4LVX1JpKB4MiosPAbpWxhr3rstQNQYrkNZDuKXujK2AfB15Fida2TtJyMtoBuod0DfQwDKYrtjWyWVBbzNPsdkHyCL4Kye1o2QqRU7ZPmrYiGWiG3A98DbQGTZijQ2wb6SjysDkUekMPLAZA4dBNifMjxF7opU2D3mj1vQfUKdgSII31EKAQ74eM2JgEOQIyAhhlR6GKZkl0KI9buqGrPrS7m9CMpZNMoJRlPtzU0n";
+const size_t contentLen  = sizeof(content)/sizeof(content[0]) ;
 char fileName[] = "test.txt";
-const char resultFile[] = "time_shared.txt";
+const char resultFile[] = "time_shared_memory.txt";
+struct timespec wStart, wStop, rStart, rStop;
 
-int main()
+typedef struct
 {
-	int shmid, status;
-	int *a, *b;
-	int i;
+  bool done;
+  pthread_mutex_t mutex;
+  char message[2048];
+} shared_data;
 
-    // tao shared memory lai IPC_PRIVATE de truyen thong tin giua cha va con 
-	shmid = shmget(IPC_PRIVATE, contentLen, 0777|IPC_CREAT);
+static shared_data* data = NULL;
 
+void initialise_shared()
+{
+    // place our shared data in shared memory
+    int prot = PROT_READ | PROT_WRITE;
+    int flags = MAP_SHARED | MAP_ANONYMOUS;
+    data = mmap(NULL, sizeof(shared_data), prot, flags, -1, 0);
+    assert(data);
 
-	if (fork() == 0) {
+    data->done = false;
 
-		/* Child Process */
+    // initialise mutex so it works properly in shared memory
+    pthread_mutexattr_t attr;
+    pthread_mutexattr_init(&attr);
+    pthread_mutexattr_setpshared(&attr, PTHREAD_PROCESS_SHARED);
+    pthread_mutex_init(&data->mutex, &attr);
+}
 
-		/*  shmat() returns a char pointer which is typecast here
-		    to int and the address is stored in the int pointer b. */
-		b = (int *) shmat(shmid, 0, 0);
+void run_child()
+{
+    pthread_mutex_lock(&data->mutex);
 
-		for( i=0; i< 10; i++) {
-			sleep(1);
-			printf("\t\t\t Child reads: %d,%d\n",b[0],b[1]);
+	// child process will write to shared process
+	clock_gettime(CLOCK_MONOTONIC, &wStart);
+	memcpy(data->message,content,contentLen);
+    data->done = true;
+	clock_gettime(CLOCK_MONOTONIC, &wStop);
+
+	FILE* result = fopen(resultFile,"a+");
+
+    fprintf(result,"(1w %ld %ld)\n", wStart.tv_sec, wStart.tv_nsec);
+    fprintf(result,"(2w %ld %ld)\n", wStop.tv_sec, wStop.tv_nsec);
+
+    pthread_mutex_unlock(&data->mutex);
+
+}
+
+// parent process will read from shared memory 
+void run_parent(pid_t pid)
+{
+	char buf[contentLen];
+
+	while (true)
+	{
+		pthread_mutex_lock(&data->mutex);
+		if (data->done) {
+			clock_gettime(CLOCK_MONOTONIC, &rStart);
+			memcpy(buf, data->message, contentLen);
+			clock_gettime(CLOCK_MONOTONIC, &rStop);
+		if (strcmp(buf, content) == 0) {
+			// ghi ket qua
+			FILE* result = fopen(resultFile,"a+");
+			fprintf(result,"(1r %ld %ld)\n", rStart.tv_sec, rStart.tv_nsec);
+			fprintf(result,"(2r %ld %ld)\n", rStop.tv_sec, rStop.tv_nsec);
+			fclose(result);
+		} else {
+			FILE* result = fopen(resultFile,"a+");
+			fprintf(result,"doc sai \n");
+			fclose(result);
 		}
-		/* each process should "detach" itself from the 
-		   shared memory after it is used */
 
-		shmdt(b);
-
-	}
-	else {
-
-		/* Parent Process */
-
-		/*  shmat() returns a char pointer which is typecast here
-		    to int and the address is stored in the int pointer a.
-		    Thus the memory locations a[0] and a[1] of the parent
-		    are the same as the memory locations b[0] and b[1] of
-		    the parent, since the memory is shared.
-		*/
-		a = (int *) shmat(shmid, 0, 0);
-
-		a[0] = 0; a[1] = 1;
-		for( i=0; i< 10; i++) {
-			sleep(1);
-			a[0] = a[0] + a[1];
-			a[1] = a[0] + a[1];
-			printf("Parent writes: %d,%d\n",a[0],a[1]);
+			pthread_mutex_unlock(&data->mutex);
+			break;
 		}
-		wait(&status);
-
-		/* each process should "detach" itself from the 
-		   shared memory after it is used */
-
-		shmdt(a);
-
-		/* Child has exited, so parent process should delete
-		   the cretaed shared memory. Unlike attach and detach,
-		   which is to be done for each process separately,
-		   deleting the shared memory has to be done by only
-		   one process after making sure that noone else
-		   will be using it 
-		 */
-
-		shmctl(shmid, IPC_RMID, 0);
+		pthread_mutex_unlock(&data->mutex);
 	}
+
+    // waitpid(pid, NULL, WNOHANG);
+}
+
+int main(int argc, char** argv)
+{
+    initialise_shared();
+
+    pid_t pid = fork();
+    if (!pid) {
+        run_child();
+    }
+    else {
+        run_parent(pid);
+    }
+
+    munmap(data, sizeof(data));
+    return 0;
 }
