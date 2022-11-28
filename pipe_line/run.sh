f03a5a091c90a6318b140aad1a6e009a9ac75f0e
ahihi8z8z <60187487+ahihi8z8z@users.noreply.github.com>
2022-11-28 15:59:03 +0700

    Update run.sh

--- pipe_line/run.sh
+++ pipe_line/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-for i in {1..100}
+echo -e "Ket qua do se co dang sau : \n Time process 1 start write to pipe: %ld second + %ld nanosecond \n Time process 1 stop write to pipe: %ld second + %ld nanosecond \n Time process 2 start read from pipe: %ld second + %ld nanosecond \n Time process 2 stop read from pipe: %ld second + %ld nanosecond \n" >> time_pipe.txt
+for i in {1..50}
 do 
 	echo -e "Lan do thu $i: " >> time_pipe.txt
 	./PipeLine 
